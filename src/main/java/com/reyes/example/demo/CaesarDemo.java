cf6b90554d57424a3367b34aa184fc4046771eea
reyes.chen <adolphgirl@gmail.com>
2022-01-11 16:27:13 +0800

    add 凱薩加密

--- src/main/java/com/reyes/example/demo/CaesarDemo.java
+++ src/main/java/com/reyes/example/demo/CaesarDemo.java
@@ -7,7 +7,33 @@ package com.reyes.example.demo;
 public class CaesarDemo {
 	
 	public static void main(String[] args) {
+		String input = "hello world";
+		String chInput = "測試中文字";
 		
+		StringBuilder crypt = new StringBuilder();
+		StringBuilder chCrypt = new StringBuilder();
+		
+//		位移3
+		int offset = 3;
+		for (char item : input.toCharArray()) {
+			int b = item;
+			b = b + offset;
+			char newItem = (char)b;
+			
+			crypt.append(newItem);
+		}
+		
+		int chOffset = 3;
+		for (char item : chInput.toCharArray()) {
+			int b = item;
+			b = b + chOffset;
+			char newItem = (char)b;
+			
+			chCrypt.append(newItem);
+		}
+		
+		System.out.println("[+] [CaesarDemo] crypt res: " + crypt.toString());
+		System.out.println("[+] [CaesarDemo] ch crypt res: " + chCrypt.toString());
 	}
 	
 }
