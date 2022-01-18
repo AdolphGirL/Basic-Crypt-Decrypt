934d50cef2f59ec1019d1a2ec038e794d0bb47a6
reyes.chen <adolphgirl@gmail.com>
2022-01-18 10:43:37 +0800

    凱薩加解密-位移法

--- src/main/java/com/reyes/example/demo/CaesarDemo.java
+++ src/main/java/com/reyes/example/demo/CaesarDemo.java
@@ -7,33 +7,55 @@ package com.reyes.example.demo;
 public class CaesarDemo {
 	
 	public static void main(String[] args) {
-		String input = "hello world";
-		String chInput = "測試中文字";
-		
-		StringBuilder crypt = new StringBuilder();
-		StringBuilder chCrypt = new StringBuilder();
-		
-//		位移3
 		int offset = 3;
-		for (char item : input.toCharArray()) {
+		
+		String input = "hello world";
+		System.out.println("[+] 原文: " + input);
+		
+		String cryptString = cryptCaesar(input, offset);
+		System.out.println("[+] 加密: " + cryptString);
+		
+		String decryptString = decryptCaesar(cryptString, offset);
+		System.out.println("[+] 解密: " + decryptString);
+	}
+	
+	/**
+	 * 加密
+	 * @param org	原文
+	 * @param key	位移量
+	 * @return
+	 */
+	public static String cryptCaesar(String org, int key) {
+		StringBuilder crypt = new StringBuilder();
+		
+		for (char item : org.toCharArray()) {
 			int b = item;
-			b = b + offset;
+			b = b + key;
 			char newItem = (char)b;
 			
 			crypt.append(newItem);
 		}
 		
-		int chOffset = 3;
-		for (char item : chInput.toCharArray()) {
-			int b = item;
-			b = b + chOffset;
-			char newItem = (char)b;
-			
-			chCrypt.append(newItem);
-		}
-		
-		System.out.println("[+] [CaesarDemo] crypt res: " + crypt.toString());
-		System.out.println("[+] [CaesarDemo] ch crypt res: " + chCrypt.toString());
+		return crypt.toString();
 	}
 	
+	/**
+	 * 解密
+	 * @param org	密文
+	 * @param key	位移量
+	 * @return
+	 */
+	public static String decryptCaesar(String org, int key) {
+		StringBuilder decrypt = new StringBuilder();
+		
+		for (char item : org.toCharArray()) {
+			int b = item;
+			b = b - key;
+			char newItem = (char)b;
+			
+			decrypt.append(newItem);
+		}
+		
+		return decrypt.toString();
+	}
 }
